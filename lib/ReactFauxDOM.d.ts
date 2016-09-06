50e54e47ef791a2450c11f86cae50bf58d33adaa
Michael Shelton <mshelton@palantir.com>
2016-09-06 17:03:22 -0400

    Messing with spaceing

--- lib/ReactFauxDOM.d.ts
+++ lib/ReactFauxDOM.d.ts
@@ -5,8 +5,8 @@ import RealElement = require("./");
  * If the source does not export function at top level,
  * you can skip the declare namespace and export what's inside directly.
  */
-type RealElement = Element;
 
+type RealElement = Element;
 declare namespace ReactFauxDOM {
   interface Element extends RealElement {
     toReact(): ReactElement<any>
