2d222354f623884e703ff9a95e328e384c55d235
Michael Shelton <mshelton@palantir.com>
2016-09-06 17:05:29 -0400

    Removign weird import

--- lib/ReactFauxDOM.d.ts
+++ lib/ReactFauxDOM.d.ts
@@ -1,12 +1,11 @@
 import {ReactElement} from "react";
-import RealElement = require("./");
 /**
  * This is just a template.
  * If the source does not export function at top level,
  * you can skip the declare namespace and export what's inside directly.
  */
+type RealElement = Element;
 
-//type RealElement = Element;
 declare namespace ReactFauxDOM {
   interface Element extends RealElement {
     toReact(): ReactElement<any>
