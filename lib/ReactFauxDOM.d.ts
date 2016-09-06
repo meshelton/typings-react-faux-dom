7c1f5d49f3469614c1ac386fb60ded134a5fd95a
Michael Shelton <mshelton@palantir.com>
2016-09-06 16:59:36 -0400

    Use typealias

--- lib/ReactFauxDOM.d.ts
+++ lib/ReactFauxDOM.d.ts
@@ -1,11 +1,14 @@
 import {ReactElement} from "react";
+import RealElement = require("./");
 /**
  * This is just a template.
  * If the source does not export function at top level,
  * you can skip the declare namespace and export what's inside directly.
  */
+type RealElement = Element;
+
 declare namespace ReactFauxDOM {
-  interface Element {
+  interface Element extends RealElement {
     toReact(): ReactElement<any>
   }
   interface Window {
