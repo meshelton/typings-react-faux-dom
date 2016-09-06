8ec5e18279d283508bffc5fdac5fe783e45525e2
Michael Shelton <mshelton@palantir.com>
2016-09-06 16:28:09 -0400

    First pass at typings

--- lib/ReactFauxDOM.d.ts
+++ lib/ReactFauxDOM.d.ts
@@ -1,10 +1,19 @@
+import {ReactElement} from "react";
 /**
  * This is just a template.
  * If the source does not export function at top level,
  * you can skip the declare namespace and export what's inside directly.
  */
 declare namespace ReactFauxDOM {
-
+  interface ZElement extends Element {
+    toReact(): ReactElement
+  }
+  interface Window {
+    getComputedStyles(node: Element): string;
+  }
+  function createElement(nodeName: string): Element
+  function createElementNS(ns: string, nodeName: string): Element
+  function compareDocumentPosition(): number
 }
 
 export = ReactFauxDOM;
